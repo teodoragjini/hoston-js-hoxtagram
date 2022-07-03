2e24f6e71f769564cd9ed3d7ecba0f333979a233
Teodora Gjini <teodoragjini@yahoo.com>
2022-07-04 01:11:04 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
-// solution goes here
+fetch("http://localhost:3002/images")
+  .then((response) => response.json())
+  .then((data) => {
+    for (let image of data) {
+      createImageCard(image);
+    }
+  });
+
+function createImageCard(image) {
+  let section = document.querySelector(".image-container");
+
+  let article = document.createElement("article");
+  article.className = "image-card";
+
+  let h2 = document.createElement("h2");
+  h2.className = "title";
+  h2.textContent = image.title;
+
+  let img = document.createElement("img");
+  img.src = image.image;
+  img.className = "image";
+
+  let div = document.createElement("div");
+  div.className = "likes-section";
+
+  let span = document.createElement("span");
+  span.className = "likes";
+  span.textContent = image.likes + " likes";
+
+  let button = document.createElement("button");
+  button.className = "like-button";
+  button.textContent = "♥";
+
+  let ul = document.createElement("ul");
+
+  for (let comment of image.comments) {
+    let li = document.createElement("li");
+    li.textContent = comment.content;
+
+    ul.append(li);
+  }
+
+  div.append(span, button);
+  article.append(h2, img, div, ul);
+  section?.append(article);
+}
